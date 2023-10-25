e8ad66a8fa4c113c87502808b062815627d3eb92
augustin <azzerty23@hotmail.fr>
2023-10-25 14:08:03 +0200

    chore: create pnpm monorepo with turbo (tooling)

--- apps/nextjs/tailwind.config.ts
+++ apps/nextjs/tailwind.config.ts
@@ -1,7 +1,10 @@
-import { type Config } from "tailwindcss";
+import type { Config } from "tailwindcss";
 import { fontFamily } from "tailwindcss/defaultTheme";
 
+import baseConfig from "@acme/tailwind-config";
+
 export default {
+  presets: [baseConfig],
   content: ["./src/**/*.tsx"],
   theme: {
     extend: {
